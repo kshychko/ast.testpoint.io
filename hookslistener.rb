590b7e59cbae668fa542fb953246bc5b25f79e64
Kseniya <k.shychko@gmail.com>
2017-02-08 16:13:48 +0300

    parse hook

--- hookslistener.rb
+++ hookslistener.rb
@@ -2,8 +2,6 @@
 require 'sinatra'
 require 'json'
 
-set :bind, '0.0.0.0'
-
 get '/' do
   "Hello World #{params[:name]}".strip
 end
@@ -11,4 +9,11 @@ end
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
+  repoURL = push["repository"]["git_url"]
+  repoName = push["repository"]["name"]
+  authorEmail = push["head_commit"]["author"]["email"]
+  authorName = push["head_commit"]["author"]["name"]
+  commitMessage = push["head_commit"]["message"]
+		
+ "Hello World #{repoURL}, #{repoName}, #{authorEmail}, #{authorName}, #{commitMessage}".strip
 end
