697c9516e0ace4f57fa387d81dc43f9530b90970
Kseniya <k.shychko@gmail.com>
2017-02-08 15:48:53 +0300

    add get method

--- hookslistener.rb
+++ hookslistener.rb
@@ -2,6 +2,12 @@
 require 'sinatra'
 require 'json'
 
+set :bind, '0.0.0.0'
+
+get '/' do
+  "Hello World #{params[:name]}".strip
+end
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
