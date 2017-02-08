cc14e39be1114f91b42ab2778293f30bf158e071
Kseniya <k.shychko@gmail.com>
2017-02-08 17:34:18 +0300

    add call sh script with open3

--- hookslistener.rb
+++ hookslistener.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby -I ../lib -I lib
 require 'sinatra'
 require 'json'
+require 'open3'
 
 set :bind, '0.0.0.0'
 
@@ -18,6 +19,18 @@ post '/payload' do
   authorEmail = push["head_commit"]["author"]["email"]
   authorName = push["head_commit"]["author"]["name"]
   commitMessage = push["head_commit"]["message"]
+
+Open3.popen3("sh sh/git-pull.sh"
+	+ ' -n ' + repoName
+	+ ' -u ' + repoURL
+	+ ' -a "' + authorName + '"'
+	+ ' -b ' + authorEmail
+	+ ' -c "' + commitMessage + '"'
+	+ ' -t ' + 'ausdigital.github.io'
+	+ ' -r ' + 'git@github.com:k.shychko/ausdigital.github.io.git') do |stdin, stdout, stderr, thread|
+   pid = thread.pid
+   puts stdout.read.chomp
+end
 
  "#{eventType} #{repoURL}, #{repoName}, #{authorEmail}, #{authorName}, #{commitMessage}".strip
 
