a0647c1bd3c1c4f2f386a0f47531628c6390ad53
Kseniya <k.shychko@gmail.com>
2017-03-22 05:04:59 +0300

    remove unnecessary dependencies

--- routes/payload.js
+++ routes/payload.js
@@ -50,8 +50,8 @@ router.post('/', function (req, res, next) {
     }
 });
 
-var repoNames = ["ausdigital.github.io", /*"ausdigital-bill", "ausdigital-dcl", */"ausdigital-dcp"/*, "ausdigital-idp", "ausdigital-nry",
-    "ausdigital-syn", "ausdigital-tap", "ausdigital-tap-gw", "ausdigital-code"*/];
+var repoNames = ["ausdigital.github.io", "ausdigital-bill", "ausdigital-dcl", "ausdigital-dcp", "ausdigital-idp", "ausdigital-nry",
+    "ausdigital-syn", "ausdigital-tap", "ausdigital-tap-gw", "ausdigital-code"];
 
 var baseDir = '/opt/'
 /*
